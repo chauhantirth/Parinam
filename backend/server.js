c99bfa54844471673ddb1d410466f3380b7a9ede
chauhantirth <chauhantirth40@gmail.com>
2024-02-03 23:42:02 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+import express from "express"
+import cors from "cors"
 
-require('dotenv').config();
+const app = express()
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(cors());
+app.use(cors())
 app.use(express.json());
 
-app.listen( port, () => {
-    console.log(`Server is running on port: ${port}`);
-})
+app.use("*", (req, res) => res.status(404).json({error: "404 not found"}))
+
+export default app
