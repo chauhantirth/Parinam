51429eaa5106b1f4dce9792c905a875c12e3bf62
chauhantirth <chauhantirth40@gmail.com>
2024-02-05 23:50:30 +0530

    added route

---
+++ backend/routes/result.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const router = express.Router()
+
+router.route('/').get(
+    (req, res) => res.send(
+        "Hello Result API"
+    )
+)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from "express"
 import cors from "cors"
+import result from "./routes/result.route.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json());
 
-app.use("/api", (req, res) => res.status(200).json({"status": "success"}))
+app.use("/api/v1/result", result)
 app.use("*", (req, res) => res.status(404).json({error: "404 not found"}))
 
 export default app
