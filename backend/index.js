7a2d24a0f2ec1e05522803156bd0d88fa29b8c32
chauhantirth <chauhantirth40@gmail.com>
2024-02-07 23:58:01 +0530

    restructured index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import result from "./routes/result.route.js"
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
@@ -17,6 +18,10 @@ MongoClient.connect(
     console.error(err.stack),
     process.exit(1)
 }).then(async client => {
+    app.use("/api/v1/result", result)
+    app.use("*", (req, res) => {
+        return res.status(404).json({ error: "404 not found" })
+    })
     app.listen(port, () => {
         console.log(`Listening on Port: ${port}`)
     })
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,9 @@
 import express from "express"
 import cors from "cors"
-import result from "./routes/result.route.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json());
-
-app.use("/api/v1/result", result)
-app.use("*", (req, res) => res.status(404).json({error: "404 not found"}))
 
 export default app
