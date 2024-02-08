fe11a79386d74f8cfd4c81ec320eb32916d869f3
chauhantirth <chauhantirth40@gmail.com>
2024-02-08 22:58:50 +0530

    added wrapper function

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
-import result from "./routes/result.route.js"
+import resultWrapper from "./routes/result.route.js"
 
 dotenv.config()
 const MongoClient = mongodb.MongoClient
@@ -18,7 +18,9 @@ MongoClient.connect(
     console.error(err.stack),
     process.exit(1)
 }).then(async client => {
-    app.use("/api/v1/result", result)
+    app.use("/api/v1/result", (req, res) => {
+        resultWrapper(req, res, MongoClient)
+    })
     app.use("*", (req, res) => {
         return res.status(404).json({ error: "404 not found" })
     })
--- backend/routes/result.route.js
+++ backend/routes/result.route.js
@@ -1,11 +1,30 @@
 import express from "express"
 
-const router = express.Router()
+function getDb(req, res, dbClient) {
+    res.send({"status": "success", "route": "result/", "method": "GET"})
+}
 
-router.route('/').get(
-    (req, res) => res.send(
-        "Hello Result API"
-    )
-)
+function postDb(dbClient) {
+    res.send({"status": "success", "route": "result/", "method": "POST"})
+}
 
-export default router
+var wrapper = function(req, res, mongoClient) {
+    var router = express.Router()
+
+    router.route('/').get(getDb(req, res, mongoClient))
+    // router.route('/').post(postDb(req, res, mongoClient))
+
+    return router
+}
+
+export default wrapper
+
+// const router = express.Router()
+
+// router.route('/').get(
+//     (req, res) => res.send(
+//         "Hello Result API"
+//     )
+// )
+
+// export default router
