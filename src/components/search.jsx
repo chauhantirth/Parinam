8958584a7889bbd0e9b927aeb34475b87ac65c06
chauhantirth <chauhantirth40@gmail.com>
2024-03-08 23:58:22 +0530

    Added form and submit button

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,9 +1,13 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import { useGlobalContext } from '../context/context';
 
 import reducer from '../reducer/searchReducer';
 
 const search = () => {
+
+  var data = useGlobalContext();
+
+  const [query, setQuery] = useState('');
 
   // bucket creation
   const initState = {
@@ -11,29 +15,31 @@ const search = () => {
     marks: 0,
     school: "",
     aadharNo: "",
+    query: "",
   };
   const [state, dispatch] = useReducer(reducer, initState);
 
   // Get Result
   const showResult = (e) => {
     e.preventDefault();
-
-    console.log(state.name)
-    return (
-      <p>
-        <strong>
-          Name is {state.name}
-        </strong>
-      </p>
-    );
+    console.log(query)
   };
 
-  var data = useGlobalContext();
   return (
     <>
       <div>
         From Search, This is a { data }
-        <button onClick={showResult}>Show Result</button>
+        <div>
+          <form onSubmit={showResult}>
+            <input
+              type="text"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Enter your Enrolment Number"
+            />
+            <button type="submit">Get Result</button>
+          </form>
+        </div>
       </div>
     </>
   )
