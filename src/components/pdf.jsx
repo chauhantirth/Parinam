87153cfec039fb5581795e2c3419d50ae0c7d2ea
chauhantirth <chauhantirth40@gmail.com>
2024-03-14 23:16:19 +0530

    Added ScoreCard Image 0

--- src/components/pdf.jsx
+++ src/components/pdf.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Page, Text, Image, Document, StyleSheet} from '@react-pdf/renderer';
 
+import ThisImage from '../assets/scoreCard/sc0.jpg';
+
 const styles = StyleSheet.create({});
 // {name, marks, schoolName}
 const PdfApp = ({name, marks, schoolName}) => {
     return (<Document>
         <Page>
-            {/* <Text>Hello1</Text> */}
-            <Text>{name}</Text>
+            <Text>Hello1</Text>
+            <Image src={ThisImage} />
+            {/* <Text>{name}</Text>
             <Text>{marks}</Text>
-            <Text>{schoolName}</Text>
+            <Text>{schoolName}</Text> */}
         </Page>
     </Document>
     )
