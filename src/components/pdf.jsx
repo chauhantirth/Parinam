73847785db301d1e6d82be885fd3d09d5fd8c100
chauhantirth <chauhantirth40@gmail.com>
2024-03-16 23:42:35 +0530

    frontend: Text Overlap login done

--- src/components/pdf.jsx
+++ src/components/pdf.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import {Page, Text, Image, Document, StyleSheet} from '@react-pdf/renderer';
+import {Page, Text, Image, Document, StyleSheet, View} from '@react-pdf/renderer';
 
-import ThisImage from '../assets/scoreCard/sc0.jpg';
+import ThisImage from '../assets/scoreCard/ddsc1.png';
 
 const styles = StyleSheet.create({});
 // {name, marks, schoolName}
 const PdfApp = ({name, marks, schoolName}) => {
-    return (<Document>
-        <Page>
-            <Text>Hello1</Text>
-            <Image src={ThisImage} />
-            {/* <Text>{name}</Text>
-            <Text>{marks}</Text>
-            <Text>{schoolName}</Text> */}
+    return (
+    <Document>
+        <Page size={"A4"} dpi={88} style={{ position: 'relative' }}>
+            <View>
+            <Text style={{ position: 'absolute', top: 0 }}>text</Text>
+            <Image src={ThisImage} debug={false} style={{ position: 'absolute', zIndex: -1, top: 0, width: '100%' }}/>   
+            </View>
         </Page>
     </Document>
     )
