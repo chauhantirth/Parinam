65a523deeb1700aaa3c38190b9142e44d6a6ab3a
chauhantirth <chauhantirth40@gmail.com>
2024-04-02 11:55:46 +0530

    vite: DDCET 2024 Live now

--- vite/src/components/constants.jsx
+++ vite/src/components/constants.jsx
@@ -1,5 +1,5 @@
 const ENV = {
-    "announcement": true,
+    "announcement": false,
     "announcementText": "DDCET Mock Test Result will be Announced on 2nd April 2024, 12:00 PM.",
     "backend": {
         "gujcet": "https://rngpit-api.vercel.app/api/v1/result/gujcet",
